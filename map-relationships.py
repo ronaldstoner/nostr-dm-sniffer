349e81cd69ee2ce1c57e043120babe088143e58d
Ron Stoner <ron@team.casa>
2023-07-14 12:47:35 -0600

    frequency colors

--- map-relationships.py
+++ map-relationships.py
@@ -2,6 +2,21 @@ import pandas as pd
 import networkx as nx
 import matplotlib.pyplot as plt
 import numpy as np
+import random
+
+# Function to assign colors for each node
+def get_colors(nodes):
+    num_nodes = len(nodes)
+    # Specify a custom color palette (add more colors if needed)
+    color_palette = ['skyblue', 'lightgreen', 'yellow', 'lightpink', 'lightcoral', 'wheat', 'mediumseagreen', 
+                     'palevioletred', 'lightsalmon', 'plum', 'lightsteelblue', 'peachpuff']
+    
+    node_color_dict = {}
+
+    for i, node in enumerate(nodes):
+        node_color_dict[node] = color_palette[i % len(color_palette)]
+
+    return node_color_dict
 
 # Load the CSV data
 df = pd.read_csv('data.csv', names=['sender_nip05', 'sender', 'receiver_nip05', 'receiver', 'timestamp'], skiprows=1)
@@ -12,14 +27,24 @@ contacts = df.groupby(['sender', 'receiver'])['sender_nip05'].count().reset_inde
 # Create directed graph
 G = nx.from_pandas_edgelist(contacts, 'sender', 'receiver', 'count', create_using=nx.DiGraph())
 
-# Create a layout for our nodes 
+# Generate random color for each node
+color_map = get_colors(G.nodes)
+
+# Create a layout for our nodes
 pos = nx.spring_layout(G, seed=123)
 
-# Plot the graph with labels
-plt.figure(figsize=(12,12))  # increase figure size
-
+# Plot the graph
 labels = nx.get_edge_attributes(G, 'count')
 
-nx.draw_networkx(G, pos, with_labels=True, node_color='skyblue', node_size=500)
-nx.draw_networkx_edge_labels(G, pos, edge_labels=labels, font_size=7)  # increase font size for readability
+plt.figure(figsize=(12,12))  # increase figure size
+
+nx.draw_networkx_nodes(G, pos, node_size=500, node_color=list(color_map.values()), alpha=0.7)
+edges = G.edges(data=True)
+edge_width = [(d['count']/np.max(contacts['count']))*5 for (u, v, d) in edges]  # Width of edge is 'count' attribute (scaled)
+nx.draw_networkx_edges(G, pos, width=edge_width, arrowstyle='->', arrowsize=10, alpha=0.5, edge_cmap=plt.cm.Blues)
+    
+# Draw the labels
+nx.draw_networkx_labels(G, pos)
+nx.draw_networkx_edge_labels(G, pos, edge_labels=labels)
+
 plt.show()
